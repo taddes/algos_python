5785547335f71960e3f95b03353f6642c259f2a3
Taddes Korris <taddes.korris@gmail.com>
2020-10-09 19:07:01 -0400

    bst

--- Binary Search Tree/binary_search_tree.py
+++ Binary Search Tree/binary_search_tree.py
@@ -5,21 +5,19 @@ class Node:
         self.left_child = None
         self.right_child = None
         self.parent = parent
-
+        
 class BinarySearchTree:
 
     def __init__(self):
         self.root = None
 
     def insert_node(self, data, node):
-        # Left subtree
         if data < node.data:
             if node.left_child:
                 self.insert_node(data, node.left_child)
             else:
                 node.left_child = Node(data, node)
-        # Right subtree
-        else:
+        elif data > node.data:
             if node.right_child:
                 self.insert_node(data, node.right_child)
             else:
@@ -30,3 +28,42 @@ class BinarySearchTree:
             self.root = Node(data, None)
         else:
             self.insert_node(data, self.root)
+
+    def traverse(self):
+        if self.root is not None:
+            self.traverse_in_order(self.root)
+
+    def traverse_in_order(self, node):
+        if node.left_child:
+            self.traverse_in_order(node.left_child)
+
+        print(node.data)
+
+        if node.right_child:
+            self.traverse_in_order(node.right_child)
+
+    def get_max_value(self):
+        if self.root:
+            return self.get_max(self.root)
+    
+    def get_max(self, node):
+        if node.right_child:
+            return self.get_max(node.right_child)
+        return node.data
+
+    def get_max_iter(self):
+        actual = self.root
+        while actual.right_child is not None:
+            actual = actual.right_child
+        
+        return actual.data
+
+
+bst = BinarySearchTree()
+bst.insert(4)
+bst.insert(5)
+bst.insert(24)
+bst.traverse()
+print(bst.get_max_value())
+bst.insert(62)
+print(bst.get_max_iter())
