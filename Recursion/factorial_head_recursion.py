87c32fd13c69be3469ffdd65981e9d0117a1162a
Taddes Korris <taddes.korris@gmail.com>
2020-12-13 17:38:44 -0500

    head and tail recursion

--- Recursion/factorial_head_recursion.py
+++ Recursion/factorial_head_recursion.py
@@ -1,1 +1,19 @@
 # Factorial example using head recursion
+# This stores each function call on the stack as it is head recursion.
+# Each function call must be evaluated and returned 
+
+#  n! = n * (n-1)!
+def factorial_head(n):
+    # base case
+    if n == 1: 
+        return 1
+
+    # use recursion
+    result1 = factorial_head(n - 1)
+    result2 = n * result1
+    return result2
+
+    # Simple implementation
+    # return n * factorial_head(n - 1)
+
+
---
+++ Recursion/factorial_tail_recursion.py
@@ -0,0 +1,7 @@
+# Tail recursion implementation
+
+def factorial_tail(n):
+
+    # base case
+    if n == 1: 
+        return 1
