77e1136960eaa24584e57374c5dc6a041bfcea1d
Taddes Korris <taddes.korris@gmail.com>
2020-12-20 14:50:03 -0500

    selection algos

--- Selection Algorithms/quickselect.py
+++ Selection Algorithms/quickselect.py
@@ -2,15 +2,15 @@
 Quickselect - Tony Hoare Algorithm
 ==================================
 Selection Algorithm - able to find the k-th smallest or largest item in an unordered array
-Has O(n) linear run time (best-case) | O(n^2) (worst-case)
+Has O(n) linear run time (best-case) | O(n^2) (worst-case) | Average O(n)
+Pivot point affects the runtime complexity. If too small.
 In-place approach - does not need additional memory or pre-sorting
 
 k-th order statistics - min, max, average, etc.
 
-Similar to Quicksort
-Instead or recursing into both sides of the array, only one side is used when
-dealing with quickselect. 
-1) Choose a pivot at random
-2) Partition the array based on the pivot value
-3) Intead of recursion into both sides, only one side
+Similar to Quicksort: Instead or recursing into both sides of the array, only one side is used when
+dealing with quickselect. Partition followed by selection.
+1) Choose a pivot at random (in the range [first_index, last_index]).
+2) Partition the array based on the pivot value.
+3) Intead of recursion into both sides, only one side (fewer recursive calls over quicksort).
 """
