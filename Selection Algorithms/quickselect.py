652487323715bdb9c8d9ab1adb29199da0d3c39d
Taddes Korris <taddes.korris@gmail.com>
2020-12-19 20:53:38 -0500

    selection algos

---
+++ Selection Algorithms/quickselect.py
@@ -0,0 +1,9 @@
+"""
+Quickselect - Tony Hoare Algorithm
+==================================
+Selection Algorithm - able to find the k-th smallest or largest item in an unordered array
+Has O(n) linear run time (best-case) | O(n^2) (worst-case)
+In-place approach - does not need additional memory or pre-sorting
+
+k-th order statistics - min, max, average, etc.
+"""
