e8a4c910fa14590dcc502f383d74e068bb69a92e
Taddes Korris <taddes.korris@gmail.com>
2020-12-19 20:55:01 -0500

    selection algos

--- Selection Algorithms/quickselect.py
+++ Selection Algorithms/quickselect.py
@@ -6,4 +6,11 @@ Has O(n) linear run time (best-case) | O(n^2) (worst-case)
 In-place approach - does not need additional memory or pre-sorting
 
 k-th order statistics - min, max, average, etc.
+
+Similar to Quicksort
+Instead or recursing into both sides of the array, only one side is used when
+dealing with quickselect. 
+1) Choose a pivot at random
+2) Partition the array based on the pivot value
+3) Intead of recursion into both sides, only one side
 """
