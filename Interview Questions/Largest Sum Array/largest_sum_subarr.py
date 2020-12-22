ef2195372cd7f73338db297f2b4f936bb9e53322
Taddes Korris <taddes.korris@gmail.com>
2020-12-22 17:54:31 -0500

    sub array largest sum

--- Interview Questions/Largest Sum Array/largest_sum_subarr.py
+++ Interview Questions/Largest Sum Array/largest_sum_subarr.py
@@ -3,4 +3,10 @@ Largest sum subarray problem
 
 Create an algorithm to find the sum of contiguous subarray within a
  one-dimensional array of numbers which has the largest sum.
+
+ Consider Kadane's Algorithm to solve this problem O(N)
+1. If the array contains all negative numbers, the solution is the number in the 
+array with the smallest magnitude.
+2. If the array contains all positive numbers, the solution is the sum of all the items.
+3. If the array contains negative and positive numbers, it is not immediately clear.
 """
