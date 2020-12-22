4ce51f6e68b807c3d8c1d5ff8bdef33297b8951d
Taddes Korris <taddes.korris@gmail.com>
2020-12-22 09:56:45 -0500

    integer reversal methods

---
+++ Interview Questions/Integer Reversion/reverse_int.py
@@ -0,0 +1,46 @@
+"""
+Interview Question - Integer Reversion
+
+Our task is to design an efficient algorithm to reverse a given integer.
+ For example if the input of the algorithm is 1234 then the output should be 4321.
+"""
+
+def rev_int(number):
+    # Easiest
+    num = str(number)[::-1]
+    print(num)
+    return int(num)
+    print(num)
+
+def rev_int2(number):
+    num = [num for num in str(number)]
+    num = ''.join(num[::-1])
+    print(num)
+    return int(num)
+
+def rev_int3(number):
+    # Overkill
+    reversed = ''
+    for char in str(number):
+        reversed = char + reversed
+    print(reversed)
+    return int(reversed)
+
+def rev_int4(number):
+    # Using modulus and the remainder by dividing by 10
+    reversed = 0
+    remainder = 0
+    while number > 0:
+        remainder = number % 10
+        number = number // 10
+        reversed = reversed * 10 + remainder
+    print(reversed)
+    return reversed
+
+
+    
+
+rev_int(4568)
+rev_int2(456856)
+rev_int3(123)
+rev_int4(123456789)
